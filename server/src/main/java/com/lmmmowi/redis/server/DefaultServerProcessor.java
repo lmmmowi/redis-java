278fc5b382f60071284acf28f6d03a5b4272c7ab
11119824 <11119824@vivo.com>
2021-08-12 16:27:38 +0800

    NettyServerProcessor通用化

--- server/src/main/java/com/lmmmowi/redis/server/DefaultServerProcessor.java
+++ server/src/main/java/com/lmmmowi/redis/server/DefaultServerProcessor.java
@@ -1,40 +1,28 @@
-package com.lmmmowi.redis.server.netty;
+package com.lmmmowi.redis.server;
 
 import com.lmmmowi.redis.protocol.command.RedisCommand;
 import com.lmmmowi.redis.protocol.reply.RedisReply;
 import com.lmmmowi.redis.protocol.reply.StatusReply;
-import com.lmmmowi.redis.server.RedisCommandLine;
-import com.lmmmowi.redis.server.ServerProcessor;
 import com.lmmmowi.redis.server.execute.RedisCommandExecutor;
 import com.lmmmowi.redis.server.execute.RedisCommandExecutors;
 import com.lmmmowi.redis.server.parser.RedisCommandParseKit;
-import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class NettyServerProcessor implements ServerProcessor {
+public class DefaultServerProcessor implements ServerProcessor {
 
-    private final NettyClientHolder nettyClientHolder = NettyClientHolder.getInstance();
     private final RedisCommandParseKit commandParseKit = RedisCommandParseKit.getInstance();
     private final RedisCommandExecutors commandExecutors = RedisCommandExecutors.getInstance();
 
     @Override
-    public void process(RedisCommandLine redisCommandLine) {
+    public RedisReply process(RedisCommandLine redisCommandLine) {
         RedisCommand command = commandParseKit.parse(redisCommandLine);
         RedisCommandExecutor executor = commandExecutors.getExecutor(command);
         if (executor != null) {
-            RedisReply reply = executor.execute(command);
-            this.writeReply(reply);
+            return executor.execute(command);
         } else {
             log.warn("can not find executor for command type: {}", command.getClass().getSimpleName());
-
-            RedisReply reply = new StatusReply("OK");
-            this.writeReply(reply);
+            return new StatusReply("OK");
         }
-    }
-
-    private void writeReply(RedisReply redisReply) {
-        Channel channel = nettyClientHolder.getClientChannel();
-        channel.writeAndFlush(redisReply);
     }
 }
