c55ce3a916384c7b8e0f2a8a8d4358c4f3987ebd
lmmmowi <lmmmowi@163.com>
2021-08-15 10:58:19 +0800

    scan方法打桩返回

--- server/src/main/java/com/lmmmowi/redis/server/execute/ScanCommandExecutor.java
+++ server/src/main/java/com/lmmmowi/redis/server/execute/ScanCommandExecutor.java
@@ -1,6 +1,5 @@
 package com.lmmmowi.redis.server.execute;
 
-import com.lmmmowi.redis.db.DbInstance;
 import com.lmmmowi.redis.db.store.DataStorage;
 import com.lmmmowi.redis.protocol.command.ScanCommand;
 import com.lmmmowi.redis.protocol.reply.ArrayReply;
@@ -8,18 +7,18 @@ import com.lmmmowi.redis.protocol.reply.FullBulkStringReply;
 import com.lmmmowi.redis.protocol.reply.RedisReply;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class ScanCommandExecutor extends AbstractCommandExecutor<ScanCommand, DataStorage> {
 
     @Override
     protected RedisReply doExecute(ScanCommand command) {
-        DbInstance db = currentDbInstance();
-        Collection<String> keys = db.scan(0);
-
-        List<RedisReply> keyReplies = keys.stream().map(FullBulkStringReply::new).collect(Collectors.toList());
+//        DbInstance db = currentDbInstance();
+//        Collection<String> keys = db.scan(0);
+//
+//        List<RedisReply> keyReplies = keys.stream().map(FullBulkStringReply::new).collect(Collectors.toList());
+        List<RedisReply> keyReplies = Collections.emptyList();
         int size = keyReplies.size();
 
         List<RedisReply> children = new ArrayList<>();
